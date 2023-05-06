615f0dabff69292a90020a03102b697104da41cc
Thanh Nguyen <ngthanh2248@gmail.com>
2023-05-06 13:10:01 -0700

    Change text color

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,13 +11,13 @@ const Hero = () => {
 				className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
 			>
 				<div className='flex flex-col justify-center items-center mt-5'>
-					<div className='w-5 h-5 rounded-full bg-[#915eff]' />
-					<div className='w-1 sm:h-80 h-40 violet-gradient' />
+					<div className='w-5 h-5 rounded-full bg-[#f61818]' />
+					<div className='w-1 sm:h-80 h-40 red-gradient' />
 				</div>
 
 				<div>
 					<h1 className={`${styles.heroHeadText} text-white`}>
-						Hi, I'm <span className='text-[#915eff]'>Thanh Nguyen</span>
+						Hi, I'm <span className='text-[#f61818]'>Thanh Nguyen</span>
 					</h1>
 					<p className={`${styles.heroSubText} mt-2 text-white-100`}>
 						I'm a CS student at the University of Waterloo,
